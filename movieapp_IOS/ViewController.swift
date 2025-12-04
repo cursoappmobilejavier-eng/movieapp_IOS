be9b918dce75d6ca2573a714442569452b70ce86
Javier <cursoappmobilejavier@gmail.com>
2025-12-04 09:39:25 +0100

    Initial Commit

---
+++ movieapp_IOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movieapp_IOS
+//
+//  Created by MananasNew on 4/12/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
